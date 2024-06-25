9a7a0751f71c9c70d44e96bd2c0a13f336b0f0e0
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-06-25 11:04:06 -0300

    criando pasta 02

---
+++ 02-introducao-typescript/tipos-dados.js
@@ -0,0 +1,16 @@
+let nome = 'Guido Cerqueira'; // string
+
+console.log(typeof nome);
+
+let idade = 33;  // number
+
+console.log(typeof idade);
+
+let altura = 1.83; // number
+
+console.log(typeof altura);
+
+let ativo = true; // boolean
+ativo = false; 
+
+console.log(typeof ativo); 
