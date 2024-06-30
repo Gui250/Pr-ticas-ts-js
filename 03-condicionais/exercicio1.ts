5c02b3c2033130d2a6e1bcd28fbbdfa87b250da2
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-06-29 21:28:00 -0300

    exercicio 1 resolvido

---
+++ 03-condicionais/exercicio1.ts
@@ -0,0 +1,9 @@
+let media: number = 7; 
+
+if(media < 4) {
+    console.log("Reprovado");
+} else if(media >= 4 && media < 7) { 
+    console.log("Recuperação");
+} else if(media >= 7) { 
+    console.log("Aprovado");
+}
