aaafe60a6e3db7a4f5969be8ff591a71f9501958
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-06-30 11:36:06 -0300

    alterações realizadas

---
+++ 03-condicionais/exemplo5.ts
@@ -0,0 +1,15 @@
+const ativo: boolean = true; 
+
+if(!ativo) { 
+    console.log("Verdadeiro");
+} else { 
+    console.log("Falso");
+}
+
+const nome = 'Pedro';
+
+if(!nome) { 
+    console.log('Verdadeiro');
+} else { 
+    console.log('Falso');
+}
