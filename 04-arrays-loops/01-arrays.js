10cadd03b8888a7c430e2b37747337a89b6f080f
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-07-10 14:26:27 -0300

    alterações realizadas

--- 04-arrays-loops/01-arrays.js
+++ 04-arrays-loops/01-arrays.js
@@ -1,2 +1,10 @@
 const listaUsuarios = ['Guido', 'João', 'Maria', 'Pedro'];
 const numeros = [123, 456, 789];
+
+console.log(listaUsuarios[0]);
+console.log(listaUsuarios);
+
+listaUsuarios[2] = 'Joana';
+listaUsuarios[4] = 'Ana';
+
+console.log(listaUsuarios);
---
+++ 04-arrays-loops/02-arrays.js
@@ -0,0 +1,7 @@
+const frutas = ['banana', 'abacaxi', 'maçã', 'pera', 'uva'];
+
+console.log(frutas.length);
+
+frutas[5] = 'mertilo';
+
+console.log(frutas.length);
