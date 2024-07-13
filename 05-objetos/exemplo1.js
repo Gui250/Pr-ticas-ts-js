ff8e9dead88b83cbe969cc8587594db8f82a7f48
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-07-13 17:27:13 -0300

    alterações feitas

--- 05-objetos/exemplo1.js
+++ 05-objetos/exemplo1.js
@@ -1,9 +1,14 @@
 
 const usuario = { 
     nome: 'Boby', 
+    email: 'teste', 
     idade: 20, 
     temCnh: true,
     habilidades: ['JavaScript', 'PHP', 'Java', 'Python']
 };
+
+usuario.nome = 'Guido Cerqueira';
+
+usuario.email = 'guido@email.com';
 
 console.log(usuario);
