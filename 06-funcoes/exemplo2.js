fbe20fcc7226b81168673734d98fa47926f5748d
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-07-20 08:59:54 -0300

    alterações feitas

--- 06-funcoes/exemplo2.js
+++ 06-funcoes/exemplo2.js
@@ -6,12 +6,26 @@ usuarios[usuarios.length] = {
     idade: 33
 };
 
-function cadastrarUsuario(nome, email, idade) { 
-    usuarios[usuarios.length] = {
-        nome: nome,
-        email: email, 
-        idade: idade
-    };
+// function cadastrarUsuario(nome, email, idade) { 
+  //  usuarios[usuarios.length] = {
+    //    nome: nome,
+     //   email: email, 
+    //    idade: idade
+  //  };
+//}
+
+function cadastrarUsuario(usuario) { 
+    usuarios[usuarios.length] = { 
+        nome: usuario.nome,
+        email: usuario.email,
+        idade: usuario.idade
+    }
 }
 
-console.log(cadastrarUsuario('Guido', 'guido@email.com', 33));
+cadastrarUsuario({
+    nome: 'Guido',
+    email: 'guido@email.com',
+    idade: 33
+})
+
+console.log(usuarios);
