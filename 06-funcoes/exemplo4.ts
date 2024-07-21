5a583f9618c971c34861ede9d7fef5fe58fcea3a
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-07-21 10:00:39 -0300

    alterações feitas

--- 06-funcoes/exemplo4.ts
+++ 06-funcoes/exemplo4.ts
@@ -3,4 +3,17 @@ function soma(numero1: number, numero2: number) {
 }
 
 
+const soma2 = (numero1: number, numero2: number) => { 
+    return numero1 + numero2; 
+}
+
+
+
+
+
+function nomeCompleto(nome: string, sobrenome: string): string { 
+    return `${nome} ${sobrenome}`
+}
+
+
 console.log(soma(200, 200));
