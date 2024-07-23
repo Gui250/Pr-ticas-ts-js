309b23f105091498237ab488b6d7e1036d941313
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-07-23 16:41:35 -0300

    alterações feitas

--- 08-tipos-utilitarios/exemplo7.ts
+++ 08-tipos-utilitarios/exemplo7.ts
@@ -8,5 +8,12 @@
 
 type TransacaoCredito = 'Credito';
 type TransacaoDebito = 'Debito';
-type Transacao = TransacaoCredito | TransacaoDebito; 
+
+
+
+type Transacao = Uppercase<TransacaoCredito> | Uppercase<TransacaoDebito>; 
+type Transacao2 = Lowercase<TransacaoCredito> | Lowercase<TransacaoDebito>;
+type Transacao3 = Capitalize<TransacaoCredito> | Capitalize<TransacaoDebito>;
+const tipo: Transacao = 'Credito';
+const tipo2: Transacao2 = 'credito';
 
