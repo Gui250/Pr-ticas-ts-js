92a46fb14aaceaf2be6936f31264e4ffa11606b7
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-07-24 15:14:46 -0300

    alterações feitas

---
+++ 09-metodos-string/exemplo3.ts
@@ -0,0 +1,15 @@
+// toUpperCase -> Transforma uma string em caracteres maisculos
+// toLowerCase -> Transforma uma string em caracteres minusculos 
+
+const texto: string = 'Guido Cerqueira';
+
+console.log(texto);
+
+const textoFormatado = texto.toUpperCase(); 
+console.log(textoFormatado);
+const parteDaString3 = texto.substring('15abc');
+console.log('A' === 'a');
+
+
+const textoFormatado2 = texto.toLowerCase(); 
+console.log(textoFormatado2);
---
+++ 09-metodos-string/exemplo4.ts
@@ -0,0 +1,14 @@
+// substring -> retorna a parte de uma string a partir do indice inicial e final 
+
+const texto = 'Guido Cerqueira'; 
+
+console.log(texto);
+
+const parteDaString = texto.substring(6, 15);
+const parteDaString2 = texto.substring(texto.length - 9); 
+const parteString3 = texto.substring(15);
+
+
+console.log(parteDaString2); 
+console.log(parteDaString); 
+console.log(parteString3);
