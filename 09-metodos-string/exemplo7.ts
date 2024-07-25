c380a8fec12fe82dd10819d5ebcdbb402b0ef556
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-07-25 11:48:54 -0300

    alterações feitas

--- 09-metodos-string/exemplo7.ts
+++ 09-metodos-string/exemplo7.ts
@@ -1,4 +1,5 @@
 // replace -> substitui uma string por outra 
+// ReplaceAll -> substitui todas correspondencias de uma string por outra 
 
 const texto: string = 'Eu adoro a Cubos Academy e adoro as aulas de lógica';
 
@@ -6,5 +7,8 @@ console.log(texto);
 
 // Substituindo a palavra adoro por amo 
 const textoSubstituido = texto.replace('adoro', 'amo');
+const textoSubstituido2 = texto.replaceAll('adoro', 'amo');
 
-console.log(textoSubstituido)
+
+console.log(textoSubstituido);
+console.log(textoSubstituido2);
