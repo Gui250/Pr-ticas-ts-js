90a14a4d1eafd6171f61238c7a92d16e16e00296
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-08-04 10:51:42 -0300

    alterações feitas

--- 12-metodos-arrays-3/exemplo2.ts
+++ 12-metodos-arrays-3/exemplo2.ts
@@ -1,3 +1,18 @@
+// Se a comparacação retornar um valor negativo, o item 1 vem antes do item 2
+// Se a comparação retornar um valor positivo, o item 2 vem antes do item 1
+// Se a comparação retornar 0, nenhum item sofre alteração
+
 const numeros = [5, 2, 40, 21, 1, 300, 3, 4]; 
 
-numeros.sort();
+// ordenação crescente
+numeros.sort(function(item1, item2) {
+    if(item1 < item2) {
+        return -1; 
+    } else if(item2 < item1) {
+        return 1; 
+    } 
+
+    return 0; 
+});
+
+console.log(numeros); 
