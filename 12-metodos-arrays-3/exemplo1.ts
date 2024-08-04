d400453cf25213efb8a6101c7ca7c369161cac29
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-08-04 10:08:41 -0300

    alterações feitas

---
+++ 12-metodos-arrays-3/exemplo1.ts
@@ -0,0 +1,12 @@
+// sort -> ordena os elementos de um array de acordo com o codigo unicode
+
+const numeros = [5, 2, 40, 21, 1, 300, 3, 4];
+const letras = ['a', 'b', 'A', 'c', 'd'];
+
+
+numeros.sort();
+letras.sort();
+
+console.log(numeros);
+console.log(letras); 
+
---
+++ 12-metodos-arrays-3/exemplo2.ts
@@ -0,0 +1,3 @@
+const numeros = [5, 2, 40, 21, 1, 300, 3, 4]; 
+
+numeros.sort();
