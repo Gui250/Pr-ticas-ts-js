571f5ead4cab4a4e5f0e268316ed796c18e461f6
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-08-07 12:03:03 -0300

    alterações feitas

--- 13-testes-automatizados/soma.test.js
+++ 13-testes-automatizados/soma.test.js
@@ -1,5 +1,11 @@
 const soma = require('./soma'); 
 
-test('deve testar o resultado da soma', () => { 
-    expect(soma(3,8)).toBe(11);
+
+describe('Testes de soma', () => { 
+    test('deve testar o resultado da soma', () => { 
+        expect(soma(3,8)).toBe(11);
+    })
+    test('deve testar o resultado incorreto de uma soma', () => { 
+        expect(soma(3,8)).not.toBe(12);
+    })
 })
