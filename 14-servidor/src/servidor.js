2e5ba2c482ce8e380fe0d9ffa29d8aaabacd558c
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-08-14 21:10:42 -0300

    alterações feitas

---
+++ 14-servidor/src/servidor.js
@@ -0,0 +1,16 @@
+import Fastify from 'fastify'
+
+const servidor = Fastify({
+    logger: true
+});
+
+
+servidor.get('/', function controlador(req, res) { 
+    const saudacao = 'Bem vindo ao primeiro servidor Node.js';
+    return res.send(saudacao); 
+})
+
+
+servidor.listen({
+    port: 3000
+})
