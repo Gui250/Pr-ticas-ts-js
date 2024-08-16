5e1df6769a2b106546ddd6cc4774120bbf3e502f
Guilherme Moreno <guilhermemoreno@Guilhermes-MacBook-Air.local>
2024-08-16 16:57:28 -0300

    testando requisições

--- 16-rotas-intermediarios/src/index.ts
+++ 16-rotas-intermediarios/src/index.ts
@@ -4,7 +4,15 @@ import express, { Request, Response } from "express";
 const app = express(); 
 
 app.get('/', (req: Request, res: Response) => {
+        console.log(req)
         return res.send('Servidor está ok!');
 })
+
+app.get('/usuarios', (req: Request, res: Response) => { 
+        console.log(req); 
+        return res.send('Servidor está ok!'); 
+})
+
+
 
 app.listen(process.env.PORT); 
